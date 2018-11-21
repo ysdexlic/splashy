07123cbdf9951220fcafd7924c4baafa1e630e3f
ysdexlic <david.arthur.thompson@gmail.com>
2018-11-21 16:42:48 +0000

    oops

--- splashy/GameScene.swift
+++ splashy/GameScene.swift
@@ -139,7 +139,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if !gameStarted {
-//            self.run(spawnDelayForever)
+            self.run(spawnDelayForever)
             gameStarted = true
         }
         enableUnderwaterPhysics = false
